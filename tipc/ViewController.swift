0028cf9c2c63112b8a4fa4239c5f18473c5084c8
Nobel Xavier <nobel_xavier@intuit.com>
2017-03-09 16:56:53 -0800

    First Commit for TipC

--- tipc/ViewController.swift
+++ tipc/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipController: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [0.18, 0.20, 0.25]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipController.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        amountLabel.text = String(format: "$%.2f", total)
+        
+    }
+    //Added this event since dragging the value changed option onto the calculateTip method was not working in my Xcode
+    @IBAction func changeTip(_ sender: Any) {
+        calculateTip(Any.self)
+    }
 }
 
